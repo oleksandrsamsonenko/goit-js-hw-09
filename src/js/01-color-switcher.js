232dd458edc857cc8be0724b26292bdc5c1b1f69
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-13 16:53:39 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+
+let intervalID;
+
+startBtn.addEventListener(`click`, onStart);
+stopBtn.addEventListener(`click`, onStop);
+
+function onStart() {
+  console.log(`interval setted to 1000ms`);
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(`color changed to -> ${document.body.style.backgroundColor}`);
+  }, 700);
+}
+
+function onStop() {
+  console.log(`interval cleared`);
+  clearInterval(intervalID);
+}
