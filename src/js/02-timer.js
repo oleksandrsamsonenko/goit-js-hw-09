0ad16e8b434c84b494eb0800c4c8577db8463400
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-16 22:36:45 +0200

    2nd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,16 +2,76 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputField = document.querySelector(`input`);
+const startBtn = document.querySelector(`button`);
+const days = document.querySelector(`[data-days]`);
+const hours = document.querySelector(`[data-hours]`);
+const minutes = document.querySelector(`[data-minutes]`);
+const seconds = document.querySelector(`[data-seconds]`);
+let selectedDate = 0;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    console.log(selectedDates);
+  onChange(selectedDates) {
+    selectedDate = selectedDates[0].getTime();
+    if (selectedDate < Date.now()) {
+      window.alert(`Please choose a date in the future`);
+      startBtn.setAttribute(`disabled`, '');
+    } else {
+      startBtn.removeAttribute(`disabled`);
+    }
   },
 };
 
-inputField.addEventListener(`click`, flatpickr('#datetime-picker', options));
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function onStartHandler() {
+  setInterval(() => {
+    const timeDifference = selectedDate - Date.now();
+    days.textContent = addLeadingZero(convertMs(timeDifference).days);
+    hours.textContent = addLeadingZero(convertMs(timeDifference).hours);
+    minutes.textContent = addLeadingZero(convertMs(timeDifference).minutes);
+    seconds.textContent = addLeadingZero(convertMs(timeDifference).seconds);
+  }, 1000);
+}
+
+startBtn.setAttribute(`disabled`, '');
+
+inputField.addEventListener(`input`, flatpickr('#datetime-picker', options));
+startBtn.addEventListener(`click`, () => onStartHandler());
+
+///styles
+const timer = document.querySelector(`.timer`);
+const field = document.querySelectorAll(`.field`);
+const value = document.querySelectorAll(`.value`);
+
+timer.style.cssText = `display: flex; gap: 30px; font-size: 36px; justify-content: center; font-weight: 600`;
+field.forEach(element => {
+  element.style.cssText = `display: flex; flex-direction: column; align-items: center`;
+});
+value.forEach(element => {
+  element.style.fontSize = `96px`;
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const delay = document.querySelector(`[name="delay"]`);
+const step = document.querySelector(`[name="step"]`);
+const amount = document.querySelector(`[name="amount"]`);
+const submitBtn = document.querySelector(`button`);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+      // Fulfill
+    } else {
+      reject({ position, delay });
+      // Reject
+    }
+  });
 }
+
+submitBtn.addEventListener(`click`, () => {
+  event.preventDefault();
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(delay.value, step.value)
+      .then(({ position, delay }) =>
+        console.log(`✅ Fulfilled promise ${i} in ${step.value * i}ms`)
+      )
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${i} in ${step.value * i}ms`)
+      );
+  }
+});
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
